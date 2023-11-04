0ac09d8d861e55770ba3d1894813eaf39641493d
outerLeitmotiv <olivier.mouttet@he-arc.ch>
2023-11-04 19:08:55 +0100

    added Readme

--- src/main/java/ch/hearc/ig/guideresto/persistence/OracleDBConnection.java
+++ src/main/java/ch/hearc/ig/guideresto/persistence/OracleDBConnection.java
@@ -3,21 +3,26 @@ package ch.hearc.ig.guideresto.persistence;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Scanner;
 
-
-/**
- * @author Olivier
- * <p>
- * GitHub link : <a href="https://github.com/outerLeitmotiv">...</a>
- */
 public class OracleDBConnection {
     private static final String DB_URL = "jdbc:oracle:thin:@db.ig.he-arc.ch:1521:ens";
-    private static final String DB_USER = "OLIVIER_MOUTTET";
-    private static final String DB_PASSWORD = "OLIVIER_MOUTTET";
+    private static String DB_USER;
+    private static String DB_PASSWORD;
     private static Connection connection;
 
     public static Connection getInstance() {
         if (connection == null) {
+            if (DB_USER == null || DB_PASSWORD == null) {
+                Scanner scanner = new Scanner(System.in);
+
+                System.out.print("Enter DB User: ");
+                DB_USER = scanner.nextLine();
+
+                System.out.print("Enter DB Password: ");
+                DB_PASSWORD = scanner.nextLine();
+            }
+
             try {
                 connection = DriverManager.getConnection(DB_URL, DB_USER, DB_PASSWORD);
             } catch (SQLException e) {
